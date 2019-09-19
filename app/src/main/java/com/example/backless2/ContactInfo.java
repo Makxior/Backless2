5d70cf6548ccf7efaa73e6d5ffb32926e08946fe
Grzegorz <grzdzik17@gmail.com>
2019-09-19 17:04:16 +0200

    ContactInfo

--- app/src/main/java/com/example/backless2/ContactInfo.java
+++ app/src/main/java/com/example/backless2/ContactInfo.java
@@ -2,13 +2,122 @@ package com.example.backless2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ContactInfo extends AppCompatActivity {
+
+    private View mProgressView;
+    private View mLoginFormView;
+    private TextView tvLoad;
+
+    TextView tvChar,tvName;
+    ImageView ivCall,ivMail,ivEdit,ivDelete;
+    EditText etName,etMail,etTel;
+    Button btnSumbit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_info);
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+        tvLoad = findViewById(R.id.tvLoad);
+
+        tvChar = findViewById(R.id.tvChar);
+        tvName = findViewById(R.id.tvName);
+
+        ivCall = findViewById(R.id.ivCall);
+        ivMail = findViewById(R.id.ivMail);
+        ivEdit = findViewById(R.id.ivEdit);
+        ivDelete = findViewById(R.id.ivDelete);
+
+        etName = findViewById(R.id.etName);
+        etMail = findViewById(R.id.etMail);
+        etTel = findViewById(R.id.etTelefon);
+
+        btnSumbit = findViewById(R.id.btnSubmit);
+
+        ivCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ivMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ivEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ivDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnSumbit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+// for very easy animations. If available, use these APIs to fade-in
+// the progress spinner.
+        if ( Build.VERSION.SDK_INT >=Build.VERSION_CODES.HONEYCOMB_MR2){
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+            tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvLoad.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else{
+// The ViewPropertyAnimator APIs are not available, so simply show
+// and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            tvLoad.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 }
